0ecbf8fe69a95a7d0c8b64f15c1b0e760e9eef96
cyc3552637 <395816504@qq.com>
2018-05-11 14:43:25 +0800

    fix project

---
+++ collectiontest/src/collectiontest/com/digital/LinkedListAdd.java
@@ -0,0 +1,17 @@
+package collectiontest.com.digital;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListAdd {
+	public List<String> add(){
+		List<String> list =  new LinkedList<String>();;
+		for(int i=0;i<120000;i++){
+			int x=(int)(Math.random()*100);
+			list.add(String.valueOf(x));
+		}
+		return list;
+	}
+
+}
